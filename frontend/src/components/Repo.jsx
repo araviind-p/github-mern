ee9c38e3446ff0244fd1010b9eca7a9611eddc50
Aravind P <aravindparavind10@gmail.com>
2024-08-21 15:21:54 +0530

    copy repo url to clipboard

--- frontend/src/components/Repo.jsx
+++ frontend/src/components/Repo.jsx
@@ -2,9 +2,19 @@ import { FaCodeBranch, FaCopy, FaRegStar } from "react-icons/fa";
 import { FaCodeFork } from "react-icons/fa6";
 import { formatDate } from "../utils/functions";
 import { PROGRAMMING_LANGUAGES } from "../utils/constants";
+import { toast } from "react-hot-toast";
 
 const Repo = ({ repo }) => {
 	const formattedDate = formatDate(repo.created_at)
+
+	const handleCLoneClick = async (repo) => {
+		try {
+			await navigator.clipboard.writeText(repo.clone_url)
+			toast.success('Repo url copied to clipboard')
+		} catch (error) {
+			toast.error('Clipboard write failed')
+		}
+	}
 	return (
 		<li className='mb-10 ms-7'>
 			<span
@@ -37,6 +47,7 @@ const Repo = ({ repo }) => {
 				<span
 					className='cursor-pointer bg-green-100 text-green-800 text-xs
 					font-medium px-2.5 py-0.5 rounded-full flex items-center gap-1'
+					onClick={() => handleCLoneClick(repo)}
 				>
 					<FaCopy /> Clone
 				</span>
