dd74a3d22bb40e4abab3ca42871a29c92b2d341c
Aravind P <aravindparavind10@gmail.com>
2024-08-23 15:53:00 +0530

    github api integrated

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -16,7 +16,11 @@ const HomePage = () => {
 		setLoading(true)
 
 		try {
-			const userRes = await fetch(`https://api.github.com/users/${username}`)
+			const userRes = await fetch(`https://api.github.com/users/${username}`,{
+				headers:{
+					authorization:`token ${import.meta.env.VITE_GITHUB_API_KEY}`
+				}
+			})
 
 			if(!userRes.ok){
 				throw new Error('User not found')
@@ -28,6 +32,7 @@ const HomePage = () => {
 			const repoRes = await fetch(userProfile.repos_url);
 			const repos = await repoRes.json()
 			setRepos(repos)
+			repos.sort((a,b)=> new Date(b.created_at) - new Date(a.created_at)) //descending
 			console.log('userProfile...', userProfile)
 			console.log('userRepos...', repos);
 			return { userProfile, repos }
@@ -55,12 +60,26 @@ const HomePage = () => {
 		setUserProfile(userProfile)
 		setRepos(repos)
 		setLoading(false)
+		setSortType('recent')
+
+	}
+
+	const onSort=(sortType)=>{
+		if(sortType === 'recent'){
+			repos.sort((a,b)=> new Date(b.created_at) - new Date(a.created_at)) //descending
+		}else if(sortType === 'stars'){
+			repos.sort((a,b)=>b.stargazers_count - a .stargazers_count) //descending , most stars first
+		}else if(sortType==='forks'){
+			repos.sort((a,b)=>b.forks_count - a.forks_count) //descending , most forks first
+		}
+		setSortType(sortType)
+		setRepos([...repos])
 	}
 
 	return (
 		<div className='m-4'>
 			<Search onSearch={onSearch} />
-			<SortRepos />
+			{repos.length > 0 && <SortRepos onSort={onSort} sortType={sortType} />}
 			<div className='flex gap-4 flex-col lg:flex-row justify-center items-start'>
 				{userProfile && !loading && <ProfileInfo userProfile={userProfile} />}
 				{!loading && <Repos repos={repos} />}
