556514211315ad03edb30950127d32dfd2b25bf2
Aravind P <aravindparavind10@gmail.com>
2024-08-28 16:02:59 +0530

    likes page

--- frontend/src/pages/LikesPage.jsx
+++ frontend/src/pages/LikesPage.jsx
@@ -53,7 +53,7 @@ const LikesPage = () => {
 	}, []);
 
 	return (
-		<div className='relative overflow-x-hidden shadow-md rounded-lg max-w-[90%]'>
+		<div className='pl-2 relative shadow-md rounded-lg '>
 			{loading && (
 				// <div className='absolute inset-0 flex items-center justify-center bg-gray-800'>
 				<Spinner />
